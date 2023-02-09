9c386d96b159b05f668524f1059e1688d99e81a0
Jared Orler <orlerdev@icloud.com>
2023-02-09 12:11:22 -0600

    Completed 'Data Types, Operators, Variables' exercise

---
+++ js/data-types-operators-variables.js
@@ -0,0 +1,164 @@
+let a = 1;
+// console.log(a); // 1
+
+let b = a++;
+// console.log(a++);
+// console.log(b); // 1
+
+let c = ++a;
+// console.log(++a);
+// console.log(c); // 3
+
+// what is the value of a, b, and c?
+
+
+let d = "hello";
+let e = false;
+
+// console.log(d++); // NaN
+// console.log(e++); // 0
+
+var perplexed; // perplexed is undefined (no value is assigned)
+perplexed + 2;
+// console.log(perplexed + 2); // NaN
+
+var price = 2.7;
+price.toFixed(2);
+// console.log(price.toFixed(2)); // 2.70
+
+var price = "2.7";
+// price.toFixed(2); // typeof string
+// console.log(price.toFixed(2)); // error, cannot call toFixed() on a string
+
+isNaN(0) // false
+
+isNaN(1) // false
+
+isNaN("") // true
+
+isNaN("string") // true
+
+isNaN("0") // true
+
+isNaN("1") // true
+
+isNaN("3.145") // true
+
+isNaN(Number.MAX_VALUE) // false
+
+isNaN(Infinity) // false, typeof Infinity === number
+
+isNaN("true") // true
+
+isNaN(true) // false
+
+isNaN("false") // true
+
+isNaN(false) // false
+
+
+// to illustrate why the isNaN() function is needed:
+NaN == NaN // false
+
+!true // false
+
+!false // true
+
+!!true // true
+
+!!false // false
+
+!!0 // false
+
+!!-0 // false
+
+!!1 // true
+
+!!-1 // true
+
+!!0.1 // true
+
+!!"hello" // true
+
+!!"" // false
+
+!!'' // false
+
+!!"false" // true
+
+!!"0" // true
+
+let sample = "Hello Codeup";
+let sampleLength = sample.length;
+// console.log(sample.toLowerCase(), sample.toUpperCase());
+
+sample = sample + " Students";
+sample = sample.replace("Students", "Class");
+
+let lowerCaseCIndex = sample.indexOf("c");
+let upperCaseCIndex = sample.indexOf("C");
+
+let request = sample.substring(upperCaseCIndex, sample.indexOf("p") + 1);
+// console.log(request);
+
+// You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay?
+let littleMermaid = 3;
+let brotherBear = 5;
+let hercules = 1;
+let totalPrice = 3 * (littleMermaid + brotherBear + hercules);
+// console.log(`The total cost of renting the movies is $${totalPrice.toFixed(2)}.`);
+
+// Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+let googlePay = 400 * 6;
+let amazonPay = 380 * 4;
+let facebookPay = 350 * 10;
+let totalPay = googlePay + amazonPay + facebookPay;
+// console.log(`Your total pay for the week would be $${totalPay.toFixed(2)}.`);
+
+// A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
+let classSpace = false;
+let schedule = "No conflict";
+let enrollment = classSpace && schedule === "No conflict" ? true : false;
+// console.log(enrollment);
+
+// A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
+let itemCount = 3;
+let offerValid = true;
+let premiumMembers = false;
+let productOffer = premiumMembers || (itemCount > 2 && offerValid) ? true : false;
+// console.log(productOffer);
+
+
+
+// Create a variable that holds a boolean value for each of the following conditions:
+//
+//   the password must be at least 5 characters
+// the password must not include the username
+// the username must be no more than 20 characters
+// neither the username or password can start or end with whitespace
+
+let username = 'codeup';
+let password = 'notastrongpassword';
+let validCharMin = true;
+let validUserNameReq = true;
+let validUserCharMax= true;
+let validPassword = true;
+let validUsername = true;
+let passCharCount = password.length;
+let userCharCount = username.length;
+
+if (password.includes(username)) {
+  validPassword = false;
+} else if (passCharCount >= 5 && password.charAt(0) !== "") {
+  validPassword = true;
+}
+
+if (userCharCount < 21 && username.charAt(0) !== "") {
+  validUsername = true;
+}
+
+console.log(validPassword, validUsername);
+
+
+
+// console.log(!!"0");
---
+++ js/javascript-intro.js
@@ -0,0 +1,67 @@
+let var1 = true;
+let var2 = 356321.01;
+let var3 = "Hello Codeup";
+let var4 = "356321.01";
+let var5 = "2" + "2";
+let var6 = 2 + 2;
+let var7 = "Jason's been drinking";
+let var8 = `You can also use back ticks instead of 'single' or "double" quotes`;
+let var08 = 'You can also escape \'single\' quotes';
+let var9 = 123;
+let var10 = false;
+let var11 = 'true';
+let var12 = true;
+let var13;
+let var14 = null;
+let var15 = "1" + 2;
+let var17 = false;
+let var18 = "Logged in user";
+let name = "Jared";
+let restrictedName = "Jared";
+let userID = 8675309;
+let postAuthor = 78394795;
+
+// console.log("3 + 4 is " + (3 + 4));
+// console.log(typeof "1" + 2);
+
+let n = 10 - 2 * 8 + 16 / 2;
+// console.log(n);
+
+let myBankAccount = 2;
+
+myBankAccount = myBankAccount + 1; // The same as myBankAccount += 1;
+myBankAccount += 1; // The same as myBankAccount = myBankAccount + 1;
+
+let price = 24.56;
+price = price.toFixed(2);
+priceInt = parseInt(price);
+priceFloat = parseFloat(price);
+
+let discordName = "[AMCX]Jared";
+let guildNameEndChar = discordName.indexOf("]") + 1;
+let guildName = discordName.substring(0, guildNameEndChar);
+discordName = discordName.substring(guildNameEndChar);
+console.log(guildName);
+console.log(discordName);
+console.log(`The player's name is ${discordName} and they are part of the ${guildName} guild.`);
+let consoleMessage = `The player's name is ${discordName} and they are part of the ${guildName} guild.`;
+console.log(consoleMessage);
+
+
+
+
+
+// console.log(typeof price);
+// console.log(typeof priceFloat);
+// console.log(priceInt);
+// console.log();
+// console.log(priceFloat)
+// console.log(userID != postAuthor);
+// console.log(name === restrictedName);
+// console.log(var17 ? "Logged in user" : "Not logged in"); // if var17 === true, log the first message, otherwise log the second message
+// console.log(var17 || var18);
+// console.log(typeof var9);
+// console.log(true && true && false); // returns false if any value is false
+// console.log(false || false || true); // returns true if any value is true
+
+
