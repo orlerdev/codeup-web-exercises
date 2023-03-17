5411762aed57762b1070367690c158d4714dd454
Jared Orler <orlerdev@icloud.com>
2023-03-17 12:41:08 -0500

    added lecture follow along and exercise files

--- js/intro-to-jquery.js
+++ js/intro-to-jquery.js
@@ -1,3 +1,40 @@
-jQuery("h3").click(function() {
-    $(this).next().slideToggle(500);
+// jQuery("h3").click(function() {
+//     $(this).next().slideToggle(500);
+// })
+
+let string = "Test string";
+
+$("h3").click(function() {
+    console.log(`You can use template literals to return ${string} values`)
+})
+
+// let libraryText = $("#library").text();
+// console.log(libraryText); //--      --////--    jQuery  --////--      --//
+// $("#library").text(`${libraryText} is a library.`) //--      --////--    #library now shows "jQuery is a library."  --////--      --//
+
+//--      --////--    JQUERY REFERENCES  --////--      --//
+// THIS ACCOMPLISHES THE SAME THING AS ABOVE
+// DOES NOT THROW AN ERROR
+let $libraryReference = $("#library");
+let libraryText = $libraryReference.text();
+console.log(libraryText);
+$libraryReference.text(`${libraryText} is a library`);
+
+//--      --////--    SETTING VARIABLES  --////--      --//
+// -logging libraryText will still return the original value of $libraryReference.text()
+// because it stores the value before it was mutated
+// -using the text() method on the element will return  the updated value
+
+console.log(libraryText); //--      --////--    jQuery  --////--      --//
+console.log($libraryReference.text()); //--      --////--    jQuery is a library  --////--      --//
+
+//--      --////--    CHANGE A SINGLE CSS PROPERTY  --////--      --//
+$("p, li").text("jQuery takes over").css("color", "hotpink");
+
+//--      --////--    CHANGE MULTIPLE CSS PROPERTIES  --////--      --//
+// -- you need to pass an object with the properties in order to change multiple properties
+$("li").css({
+    "color": "dodgerblue",
+    "font-weight": "bold",
+    "font-size": "36px"
 })
---
+++ js/jquery-exercise.js
@@ -0,0 +1,9 @@
+$("li").css("font-size", "20px");
+$("h1").css("background-color", "rgb(255,0,0,0.1");
+$("p").css("background-color", "pink");
+$("li").css("background-color", "goldenrod");
+$("h1, p, li").css("background-color", "rgba(160,225,120,.6");
+
+
+
+// alert($("h1").text());
