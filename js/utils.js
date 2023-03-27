d2defc0298a0cc787b603075407ebe9215b442f4
Jared Orler <orlerdev@icloud.com>
2023-03-27 14:39:00 -0500

    fixed fetch

--- js/utils.js
+++ js/utils.js
@@ -13,6 +13,20 @@ function setTime() {
     return { time, month, day, date, hours, hoursForClock, minutes, seconds, ampm, days, months };
 }
 
+function getAPIDate(unix) {
+    const dateForDisplay = new Date(unix * 1000);
+    const month = dateForDisplay.getMonth();
+    const day = dateForDisplay.getDay();
+    const date= dateForDisplay.getDate();
+    const hours = dateForDisplay.getHours();
+    const hoursForClock = hours >= 13 ? hours % 12 : hours;
+    const minutes = dateForDisplay.getMinutes();
+    const seconds = dateForDisplay.getSeconds();
+    const ampm = hours >= 12 ? "pm" : "am";
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return { dateForDisplay, month, day, date, hours, hoursForClock, minutes, seconds, ampm, days, months };
+}
 
 function reformatDate(date) {
     let today = new Date();
@@ -42,9 +56,9 @@ function removeDuplicates(arr) {
     }
 }
 
-const setStylesOnElement = function(styles, element){
+const setStylesOnElement = function (styles, element) {
     Object.assign(element.style, styles);
-}
+};
 
 
 
--- js/weather_map.js
+++ js/weather_map.js
@@ -1,23 +1,8 @@
-(() => {
-
+(async () => {
+    //<--VARIABLES-->
     const SAlong = -98.48527;
     const SAlat = 29.423017;
     const imperial = "imperial";
-
-    (async () => {
-        const fetchWeather = async () => {
-            try {
-                const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${SAlat}&lon=${SAlong}&units=${imperial}&appid=${OWEATHER_KEY}`);
-                const data = await res.json();
-                console.log(data);
-                return data;
-            } catch (e) {
-                console.log(e.message);
-            }
-        };
-    })();
-
-//<--VARIABLES-->
     const pageContainer = document.querySelector(".page-container");
     const forecastShowBtn = document.querySelector(".five-day-btn");
     const mapShowBtn = document.querySelector(".map-show-btn");
@@ -52,17 +37,15 @@
     const currentPop = document.querySelector(".current-pop");
     const mapContainer = document.querySelector(".map-container");
 
-//<--PHOTO ARRAYS-->
-    const sunny = ["url('./img/sunny/${randomNum}.png')"];
-    const hotDays = ["url('../../assets/img/')"];
-    const windyDays = ["url('../../assets/img/')"];
-    const cloudyDays = ["url('../../assets/img/')"];
-    const rainyDaysUrl = ["url('../../assets/img/')"];
-    const snowyDays = ["url('../../assets/img/')"];
-    const freezingDays = ["url('../../assets/img/')"];
-    const thunderStorms = ["url('../../assets/img/')"];
-    const snowStorms = ["url('../../assets/img/')"];
-    const coldDays = ["url('../../assets/img/')"];
+    const fetchWeather = async () => {
+        try {
+            const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${SAlat}&lon=${SAlong}&units=${imperial}&appid=${OWEATHER_KEY}`);
+            const data = await res.json();
+            return data;
+        } catch (e) {
+            console.log(e.message);
+        }
+    };
 
     const getPhoto = (weather, num) => {
         let weatherArr = [];
@@ -77,26 +60,42 @@
 
 //<--ARRAYS-->
     let { day, date, days, month, months, hoursForClock, minutes, ampm } = setTime();
+    let { dateForDisplay, monthA, dayA, dateA, hours, hoursForClockA, minutesA, secondsA, ampmA, daysA, monthsA } = getAPIDate();
     const scale = (num, in_min, in_max, out_min, out_max) => {
         return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
     };
     const indicatorTabs = document.querySelectorAll(".indicator-tab");
+    const sunny = ["url('./img/sunny/${randomNum}.png')"];
+    const hotDays = ["url('../../assets/img/')"];
+    const windyDays = ["url('../../assets/img/')"];
+    const cloudyDays = ["url('../../assets/img/')"];
+    const rainyDaysUrl = ["url('../../assets/img/')"];
+    const snowyDays = ["url('../../assets/img/')"];
+    const freezingDays = ["url('../../assets/img/')"];
+    const thunderStorms = ["url('../../assets/img/')"];
+    const snowStorms = ["url('../../assets/img/')"];
+    const coldDays = ["url('../../assets/img/')"];
 
 //<--LOADING BACKGROUND-->
     let load = 0;
     let int = setInterval(blurring, 15);
 
-    function blurring() {
+    async function blurring() {
         load++;
         if (load > 99) {
             clearInterval(int);
         }
         currentDate.innerText = `${days[day]} ${months[month]}, ${date}`;
         currentTime.textContent = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
-        currentTemp.innerText = `${72}ºF`;
-        currentPop.innerText = `${30}% chance of ${"rain"}`;
         pageContainer.style.filter = `blur(${scale(load, 0, 100, 15, 0)}px)`;
     }
+
+    async function getCurrentTemp() {
+        let data = await fetchWeather();
+        return currentTemp.innerText = `${Math.floor(data.current.temp)}ºF`;
+    }
+
+    await getCurrentTemp();
 
 //<--BACKGROUNDS-->
 
@@ -151,46 +150,51 @@
 
     });
 
-    forecastShowBtn.addEventListener("click", () => {
-        async function useWeather() {
-            let weather = await fetchWeather();
-            let currentTemperature = await weather.current.temp;
-            let currentUvi = await weather.current.uvi;
-            let currentpop = await weather.current.pop;
-            let currentSunrise = await weather.current.sunrise;
-            let currentSunset = await weather.current.sunset;
-            let currentPressure = await weather.current.pressure;
-            let currentHumidity = await weather.current.humidity;
-            let currentClouds = await weather.current.clouds;
-            let currentVisibility = await weather.current.visibility;
-            let currentDewPoint = await weather.current.dew_point;
-            let currentWeather = await weather.current.weather.main;
-            let currentWeatherDesc = await weather.current.weather.description;
-            // let dayDate = await weather.daily.dt;
-            let dailyTemp = await weather.daily.temp.max;
-            let dailySunrise = await weather.daily.sunrise;
-            let dailySunset = await weather.daily.sunset;
-            let dailyPressure = await weather.daily.pressure;
-            let dailyHumidity = await weather.daily.humidity;
-            let dailyClouds = await weather.daily.clouds;
-            let dailyVisibility = await weather.daily.visibility;
-            let dailyDewPoint = await weather.daily.dew_point;
-            let dailytUvi = await weather.daily.uvi;
-            let dailytpop = await weather.daily.pop;
-            let dailyWeather = await weather.daily.weather.main;
-            let dailyWeatherDesc = await weather.daily.weather.description;
-            cardDate.innerText = `${weather.daily.dt}`;
-            sunriseValue.innerText = `${dailySunrise}`;
-            uviValue.innerText = `${dailyUvi}`;
-            humidityValue.innerText = `${dailyHumidity}`;
-            cloudsValue.innerText = `${dailyClouds}`;
-            sunsetValue.innerText = `${dailySunset}`;
-            pressureValue.innerText = `${dailyPressure}`;
-            visibilityValue.innerText = `${dailyVisibility}`;
-            dewPointValue.innerText = `${dailyDewPoint}`;
-            cardDescription.innerText = `${dailyWeatherDesc}`;
+    forecastShowBtn.addEventListener("click", async () => {
+        let data = await fetchWeather();
+        let date = new Date(data.current.dt * 1000);
+        console.log(data);
+        console.log(date);
+
+        function getCardTime(unix) {
+            let dt = new Date(unix);
+            const hours = dt.getHours();
+            const hoursForClock = hours >= 13 ? hours % 12 : hours;
+            const minutes = dt.getMinutes();
+            const seconds = dt.getSeconds();
+            const ampm = hours >= 12 ? "pm" : "am";
+            return `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
         }
-            forecastContainer.classList.toggle("active");
+
+        sunriseValue.innerText = `${getCardTime(data.current.sunrise)}`;
+        uviValue.innerText = `${data.current.uvi}`;
+        humidityValue.innerText = `${data.current.humidity}%`;
+        cloudsValue.innerText = `${data.current.clouds}%`;
+        sunsetValue.innerText = `${getCardTime(data.current.sunset)}`;
+        pressureValue.innerText = `${data.current.pressure}pHa`;
+        visibilityValue.innerText = `${Math.floor(data.current.visibility / 1609)}mile(s)`;
+        dewPointValue.innerText = `${Math.floor(data.current.dew_point)}ºF`;
+        cardDescription.innerText = `${data.current.weather.description}`;
+        cardDate.innerText = `${date.getMonth() + 1} / ${date.getDate()}`;
+        tab1.innerText = `${date.getMonth() + 1} / ${date.getDate()}`;
+        date = new Date(data.daily[1].dt * 1000);
+        tab2.innerText = `${date.getMonth() + 1} / ${date.getDate()}`;
+        date = new Date(data.daily[2].dt * 1000);
+        tab3.innerText = `${date.getMonth() + 1} / ${date.getDate()}`;
+        date = new Date(data.daily[3].dt * 1000);
+        tab4.innerText = `${date.getMonth() + 1} / ${date.getDate()}`;
+        date = new Date(data.daily[4].dt * 1000);
+        tab5.innerText = `${date.getMonth() + 1} / ${date.getDate()}`;
+        date = new Date(data.daily[5].dt * 1000);
+        forecastContainer.classList.toggle("active");
+    });
+
+    let tabs = [...document.querySelectorAll(".indicator-tab")];
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            cardDate.innerText = tab.innerText;
+            sunriseValue.innerText = "";
+        });
     });
 
     mapShowBtn.addEventListener("click", () => {
@@ -216,6 +220,42 @@
     indicatorTab.addEventListener("click", () => {
 
     });
-
 })();
+// })();
+
+// let currentTemperature = await weather.current.temp;
+// let currentUvi = await weather.current.uvi;
+// let currentpop = await weather.current.pop;
+// let currentSunrise = await weather.current.sunrise;
+// let currentSunset = await weather.current.sunset;
+// let currentPressure = await weather.current.pressure;
+// let currentHumidity = await weather.current.humidity;
+// let currentClouds = await weather.current.clouds;
+// let currentVisibility = await weather.current.visibility;
+// let currentDewPoint = await weather.current.dew_point;
+// let currentWeather = await weather.current.weather.main;
+// let currentWeatherDesc = await weather.current.weather.description;
+// // let dayDate = await weather.daily.dt;
+// let dailyTemp = await weather.daily.temp.max;
+// let dailySunrise = await weather.daily.sunrise;
+// let dailySunset = await weather.daily.sunset;
+// let dailyPressure = await weather.daily.pressure;
+// let dailyHumidity = await weather.daily.humidity;
+// let dailyClouds = await weather.daily.clouds;
+// let dailyVisibility = await weather.daily.visibility;
+// let dailyDewPoint = await weather.daily.dew_point;
+// let dailytUvi = await weather.daily.uvi;
+// let dailytpop = await weather.daily.pop;
+// let dailyWeather = await weather.daily.weather.main;
+// let dailyWeatherDesc = await weather.daily.weather.description;
+// cardDate.innerText = `${weather.daily.dt}`;
+// sunriseValue.innerText = `${dailySunrise}`;
+// uviValue.innerText = `${dailyUvi}`;
+// humidityValue.innerText = `${dailyHumidity}`;
+// cloudsValue.innerText = `${dailyClouds}`;
+// sunsetValue.innerText = `${dailySunset}`;
+// pressureValue.innerText = `${dailyPressure}`;
+// visibilityValue.innerText = `${dailyVisibility}`;
+// dewPointValue.innerText = `${dailyDewPoint}`;
+// cardDescription.innerText = `${dailyWeatherDesc}`;
 
