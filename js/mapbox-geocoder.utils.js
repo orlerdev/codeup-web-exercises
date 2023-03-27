c46545094a73bcff6332eea3bff560425af12f83
Jared Orler <orler@Jareds-MBP.lan>
2023-03-27 02:33:38 -0500

    updates to all files

--- js/mapbox-geocoder.utils.js
+++ js/mapbox-geocoder.utils.js
@@ -14,12 +14,12 @@
  *
  */
 function geocode(search, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
+    const baseUrl = 'https://api.mapbox.com';
+    const endPoint = '/geocoding/v5/mapbox.places/';
     return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
         .then(function(res) {
             return res.json();
-        // to get all the data from the request, comment out the following three lines...
+            // to get all the data from the request, comment out the following three lines...
         }).then(function(data) {
             return data.features[0].center;
         });
@@ -40,8 +40,8 @@ function geocode(search, token) {
  *
  */
 function reverseGeocode(coordinates, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
+    const baseUrl = 'https://api.mapbox.com';
+    const endPoint = '/geocoding/v5/mapbox.places/';
     return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
         .then(function(res) {
             return res.json();
--- js/weather_map.js
+++ js/weather_map.js
@@ -1,114 +1,221 @@
-//--      --////--    OPENWEATHERMAP API fetch FOR WEATHER  --////--      --//
-// (async () => {
-//     const longSA = -98.4946;
-//     const latSA = 29.4252;
-//     const imperial = 'imperial';
-//     const fetchSanAntonio = async () => {
-//         try {
-//             const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latSA}&lon=${longSA}&units=imperial&appid=${OWEATHER_KEY}`);
-//             const data = await res.json();
-//             console.log(data);
-//             document.querySelector('h1').innerHTML = `${data.current.temp}`;
-//             console.log(`The current temp is: ${data.current.temp}`);
-//         } catch (e) {
-//             console.log(e.message);
-//         }
-//     };
-//     await fetchSanAntonio();
-// })();
-//--      --////--    VARIABLES  --////--      --//
+(() => {
 
-//--      --////--    DATE & TIME VARIABLES  --////--      --//
-const headerDay = document.querySelector(".header-day");
-const headerDate = document.querySelector(".header-date");
-const headerTime = document.querySelector(".header-time");
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const SAlong = -98.48527;
+    const SAlat = 29.423017;
+    const imperial = "imperial";
 
-//--      --////--    LEFT SECTION VARIABLES  --////--      --//
-const slideContainer = document.querySelector(".slider-container");
-const slideRight = document.querySelector(".right-slide");
-const slideLeft = document.querySelector(".left-slide");
-const upBtn = document.querySelector(".up-button");
-const downBtn = document.querySelector(".down-button");
-const slidesLength = slideRight.querySelectorAll("div").length;
-const tabContainer = document.querySelector(".day-indicator-col");
+    (async () => {
+        const fetchWeather = async () => {
+            try {
+                const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${SAlat}&lon=${SAlong}&units=${imperial}&appid=${OWEATHER_KEY}`);
+                const data = await res.json();
+                console.log(data);
+                return data;
+            } catch (e) {
+                console.log(e.message);
+            }
+        };
+    })();
 
-//--      --////--    ARRAYS  --////--      --//
-const indicatorArray = [...document.querySelectorAll(".indicator-tab")];
-let indicatorsLength = indicatorArray.length;
+//<--VARIABLES-->
+    const pageContainer = document.querySelector(".page-container");
+    const forecastShowBtn = document.querySelector(".five-day-btn");
+    const mapShowBtn = document.querySelector(".map-show-btn");
+    const forecastContainer = document.querySelector(".forecast-container");
+    const forecastCard = document.querySelector(".forecast-card");
+    const cardInfo = document.querySelector(".card-info");
+    const cardHead = document.querySelector(".card-head");
+    const cardDate = document.querySelector(".card-date");
+    const cardBody = document.querySelector(".card-body");
+    const sunriseValue = document.querySelector("#sunrise-value");
+    const uviValue = document.querySelector("#uvi-value");
+    const humidityValue = document.querySelector("#humidity-value");
+    const cloudsValue = document.querySelector("#clouds-value");
+    const sunsetValue = document.querySelector("#sunset-valule");
+    const pressureValue = document.querySelector("#pressure-value");
+    const visibilityValue = document.querySelector("#visibility-value");
+    const dewPointValue = document.querySelector("#dew-point-value");
+    const cardDescription = document.querySelector(".card-description");
+    const forecastIndicatorContainer = document.querySelector(".forecast-indicator");
+    const indicatorTab = document.querySelector(".indicator-tab");
+    const tab1 = document.querySelector("#tab1");
+    const tab2 = document.querySelector("#tab2");
+    const tab3 = document.querySelector("#tab3");
+    const tab4 = document.querySelector("#tab4");
+    const tab5 = document.querySelector("#tab5");
+    const searchBtn = document.querySelector(".search-btn");
+    const searchInput = document.querySelector("#hidden-search");
+    const hiddenSearch = document.querySelector(".search");
+    const currentDate = document.querySelector(".current-date");
+    const currentTime = document.querySelector(".current-time");
+    const currentTemp = document.querySelector(".current-temp");
+    const currentPop = document.querySelector(".current-pop");
+    const mapContainer = document.querySelector(".map-container");
 
-//--      --////--    PHOTO ARRAYS  --////--      --//
-const sunnyDays = [];
-const hotDays = [];
-const windyDays = [];
-const cloudyDays = [];
-const rainyDaysUrl = [];
-const snowyDays = [];
-const freezingDays = [];
-const thunderStorms = [];
-const snowStorms = [];
-const coldDays = [];
+//<--PHOTO ARRAYS-->
+    const sunny = ["url('./img/sunny/${randomNum}.png')"];
+    const hotDays = ["url('../../assets/img/')"];
+    const windyDays = ["url('../../assets/img/')"];
+    const cloudyDays = ["url('../../assets/img/')"];
+    const rainyDaysUrl = ["url('../../assets/img/')"];
+    const snowyDays = ["url('../../assets/img/')"];
+    const freezingDays = ["url('../../assets/img/')"];
+    const thunderStorms = ["url('../../assets/img/')"];
+    const snowStorms = ["url('../../assets/img/')"];
+    const coldDays = ["url('../../assets/img/')"];
 
-//--      --////--    UTILITY FUNCTIONS  --////--      --//
-const randomNumberRange = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-
-downBtn.addEventListener("click", () => changeSlide("down"));
-upBtn.addEventListener("click", () => changeSlide("up"));
-slideLeft.style.top = `-${(slidesLength - 1) * 100}%`;
-
-const getWeatherPhoto = (weatherArr, randomNum) => {
-    return `url('weatherArr[randomNum]'`;
-};
-
-let activeSlide = 0;
-const changeSlide = (direction) => {
-    const sliderHeight = slideContainer.clientHeight;
-    if (direction === "down") {
-        activeSlide++;
-        if (activeSlide > slidesLength - 1) {
-            activeSlide = 0;
-            slideLeft.style.transition = "none";
+    const getPhoto = (weather, num) => {
+        let weatherArr = [];
+        for (let i = 1; i <= num; i++) {
+            weatherArr.push("url(`./img/sunny/${num}.png`)");
+            i++;
+            num++;
         }
-    } else if (direction === "up") {
-        activeSlide--;
-        if (activeSlide < 0) {
-            activeSlide = slidesLength - 1;
-            slideLeft.style.transition = "none";
+        console.log(weatherArr);
+        return weatherArr;
+    };
+
+//<--ARRAYS-->
+    let { day, date, days, month, months, hoursForClock, minutes, ampm } = setTime();
+    const scale = (num, in_min, in_max, out_min, out_max) => {
+        return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+    };
+    const indicatorTabs = document.querySelectorAll(".indicator-tab");
+
+//<--LOADING BACKGROUND-->
+    let load = 0;
+    let int = setInterval(blurring, 15);
+
+    function blurring() {
+        load++;
+        if (load > 99) {
+            clearInterval(int);
         }
+        currentDate.innerText = `${days[day]} ${months[month]}, ${date}`;
+        currentTime.textContent = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
+        currentTemp.innerText = `${72}ºF`;
+        currentPop.innerText = `${30}% chance of ${"rain"}`;
+        pageContainer.style.filter = `blur(${scale(load, 0, 100, 15, 0)}px)`;
     }
-    slideRight.style.transform = `translateY(-${activeSlide * sliderHeight}px)`;
-    slideLeft.style.transform = `translateY(${activeSlide * sliderHeight}px)`;
-    slideRight.style.backgroundImage = 'url("../../assets/img/sunset-cliff.jpeg")';
-    indicatorArray.forEach(tab => tab.classList.remove("active"));
-    indicatorArray[activeSlide].classList.add("active");
-    setTimeout(() => {
-        slideLeft.style.transition = "";
-    }, 800);
-};
 
-const setStylesOnElement = function(styles, element){
-    Object.assign(element.style, styles);
-}
+//<--BACKGROUNDS-->
 
-function setTime() {
-    const time = new Date();
-    const month = time.getMonth();
-    const day = time.getDay();
-    const date = time.getDate();
-    const hours = time.getHours();
-    const hoursForClock = hours >= 13 ? hours % 12 : hours;
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
-    const ampm = hours >= 12 ? "PM" : "AM";
-    headerDay.innerHTML = `${days[day]}`;
-    headerDate.innerHTML = `${months[month]}, ${date}`;
-    headerTime.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
-};
-setTime();
-setInterval(setTime, 60000);
+    let randomNumClouds = randomNumber(1, 8);
+    let randomNumFogMist = randomNumber(1, 4);
+    let randomNumNight = randomNumber(1, 14);
+    let randomNumRain = randomNumber(1, 14);
+    let randomNumSnow = randomNumber(1, 21);
+    let randomNumSunny = randomNumber(1, 18);
+    let randomNumSunrise = randomNumber(1, 10);
+    let randomNumSunset = randomNumber(1, 6);
+    let randomNumStorm = randomNumber(1, 6);
 
-//TODO: create function to retrieve weather, use default longLat values
+    pageContainer.style.backgroundImage = `url('./img/sunny/${randomNumSunny}.png`;
+    forecastCard.style.backgroundImage = `url('./img/rain/${randomNumRain}.png`;
 
-//TODO: create a function for geocoding calls
+//<--MAP FETCH-->
+//<--EVENT LISTENERS-->
+
+    searchBtn.addEventListener("click", (e) => {
+        console.log("event fired");
+        console.log(searchInput.value.length);
+        if (searchInput.value.length === 0) {
+            console.log("event fired");
+            hiddenSearch.classList.toggle("active");
+            searchInput.focus();
+        }
+    });
+
+    searchInput.addEventListener("input", e => {
+        let address = searchInput.value;
+        console.log(address);
+        geocode(address, MAPBOX_API_TOKEN).then(coordinates => {
+            const newMarker = new mapboxgl.Marker({
+                "draggable": "true"
+            })
+                .setLngLat(coordinates)
+                .addTo(map);
+            map.setCenter(coordinates);
+        });
+        mapContainer.classList.add("active");
+    });
+
+    document.addEventListener("click", e => {
+        const isClickInside = searchInput.contains(e.target);
+        if (!isClickInside) {
+            searchInput.classList.remove("active");
+        }
+    });
+
+    document.addEventListener("click", e => {
+
+    });
+
+    forecastShowBtn.addEventListener("click", () => {
+        async function useWeather() {
+            let weather = await fetchWeather();
+            let currentTemperature = await weather.current.temp;
+            let currentUvi = await weather.current.uvi;
+            let currentpop = await weather.current.pop;
+            let currentSunrise = await weather.current.sunrise;
+            let currentSunset = await weather.current.sunset;
+            let currentPressure = await weather.current.pressure;
+            let currentHumidity = await weather.current.humidity;
+            let currentClouds = await weather.current.clouds;
+            let currentVisibility = await weather.current.visibility;
+            let currentDewPoint = await weather.current.dew_point;
+            let currentWeather = await weather.current.weather.main;
+            let currentWeatherDesc = await weather.current.weather.description;
+            // let dayDate = await weather.daily.dt;
+            let dailyTemp = await weather.daily.temp.max;
+            let dailySunrise = await weather.daily.sunrise;
+            let dailySunset = await weather.daily.sunset;
+            let dailyPressure = await weather.daily.pressure;
+            let dailyHumidity = await weather.daily.humidity;
+            let dailyClouds = await weather.daily.clouds;
+            let dailyVisibility = await weather.daily.visibility;
+            let dailyDewPoint = await weather.daily.dew_point;
+            let dailytUvi = await weather.daily.uvi;
+            let dailytpop = await weather.daily.pop;
+            let dailyWeather = await weather.daily.weather.main;
+            let dailyWeatherDesc = await weather.daily.weather.description;
+            cardDate.innerText = `${weather.daily.dt}`;
+            sunriseValue.innerText = `${dailySunrise}`;
+            uviValue.innerText = `${dailyUvi}`;
+            humidityValue.innerText = `${dailyHumidity}`;
+            cloudsValue.innerText = `${dailyClouds}`;
+            sunsetValue.innerText = `${dailySunset}`;
+            pressureValue.innerText = `${dailyPressure}`;
+            visibilityValue.innerText = `${dailyVisibility}`;
+            dewPointValue.innerText = `${dailyDewPoint}`;
+            cardDescription.innerText = `${dailyWeatherDesc}`;
+        }
+            forecastContainer.classList.toggle("active");
+    });
+
+    mapShowBtn.addEventListener("click", () => {
+        mapContainer.classList.toggle("active");
+        if (forecastContainer.classList.contains("active")) {
+            forecastContainer.classList.remove("active");
+        }
+    });
+
+    const removeActiveClass = () => {
+        indicatorTabs.forEach(tab => {
+            tab.classList.remove("active");
+        });
+    };
+
+    indicatorTabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            removeActiveClass();
+            tab.classList.add("active");
+        });
+    });
+
+    indicatorTab.addEventListener("click", () => {
+
+    });
+
+})();
 
