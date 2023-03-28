f79546d8a41d44f19dc8344439e94dac0945adb7
Jared Orler <orlerdev@icloud.com>
2023-03-28 14:56:02 -0500

    restored utils.js file

--- js/utils.js
+++ js/utils.js
@@ -1,22 +1,37 @@
-export function setDateAndTime(dt) {
-    const timestamp = new Date();
-    const month = timestamp.getMonth();
-    const day = timestamp.getDay();
-    const date = timestamp.getDate();
-    const hours = timestamp.getHours();
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const date = time.getDate();
+    const hours = time.getHours();
     const hoursForClock = hours >= 13 ? hours % 12 : hours;
-    const minutes = timestamp.getMinutes();
-    const seconds = timestamp.getSeconds();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
     const ampm = hours >= 12 ? "pm" : "am";
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    return { timestamp, month, day, date, hours, hoursForClock, minutes, seconds, ampm, days, months };
+    return { time, month, day, date, hours, hoursForClock, minutes, seconds, ampm, days, months };
 }
 
-export function reformatDate(date) {
+function getAPIDate(unix) {
+    const dateForDisplay = new Date(unix * 1000);
+    const month = dateForDisplay.getMonth();
+    const day = dateForDisplay.getDay();
+    const date= dateForDisplay.getDate();
+    const hours = dateForDisplay.getHours();
+    const hoursForClock = hours >= 13 ? hours % 12 : hours;
+    const minutes = dateForDisplay.getMinutes();
+    const seconds = dateForDisplay.getSeconds();
+    const ampm = hours >= 12 ? "pm" : "am";
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return { dateForDisplay, month, day, date, hours, hoursForClock, minutes, seconds, ampm, days, months };
+}
+
+function reformatDate(date) {
     let today = new Date();
     let year = today.getFullyYear();
-    let dateParts = today.split(" ");
+    let dateParts = date.split(" ");
     let month = dateParts[1].split("/")[0];
     let day = dateParts[1].split("/")[1];
     let newDateString = `${month}/${day}/${year}`;
@@ -27,29 +42,21 @@ export function reformatDate(date) {
     return newDateString;
 }
 
-export function formatCurrency(num, lang = "en", country = "US", style = "currency", currency = "USD") {
+function formatCurrency(num, lang = "en", country = "US", style = "currency", currency = "USD") {
     return parseFloat(num).toLocaleString(`${lang}-${country}`, { style: style, currency: currency });
 }
 
-export function randomNumber(min, max) {
+function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-export function removeDuplicates(arr) {
+function removeDuplicates(arr) {
     if (!Array.isArray(arr)) {
         return [...new Set(arr)];
     }
 }
 
-export const setStylesOnElement = function (styles, element) {
+const setStylesOnElement = function (styles, element) {
     Object.assign(element.style, styles);
 };
 
-
-const codeUp = {frontend: "javascript", backend: "java", database: "mysql"};
-const {frontend, backend, database } = codeUp;
-const {frontend: firsthalf, backend: backhalf, database: collection} = codeUp;
-
-console.log(codeUp);
-console.log(`${frontend} ${backend} ${database}`);
-console.log(`${firsthalf} ${backhalf} ${collection}`);
